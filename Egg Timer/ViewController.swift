df663eae2d41a3ec04ba88f5e1e5d344117a0d0b
Yara TP <ytercero36@gmail.com>
2016-03-10 23:01:25 -0500

    Timer files first commit

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,10 +9,98 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var timerTextField: UILabel!
+    
+    var timer = NSTimer()
+    var seconds = 0
+    var minutes = 0
+    
+    
+    func result(){
+        
+        seconds += 1
 
+        
+        if seconds < 60 {
+            
+            if seconds < 10 && minutes < 10 {
+                
+                return timerTextField.text = "0\(minutes) : 0\(seconds)"
+                
+            } else if seconds < 10 && minutes >= 10 {
+                
+                return timerTextField.text = "\(minutes) : 0\(seconds)"
+                
+            } else if seconds >= 10 && minutes < 10 {
+                
+                return timerTextField.text = "0\(minutes) : \(seconds)"
+                
+            } else {
+                
+                return timerTextField.text = "\(minutes) : \(seconds)"
+                
+            }
+            
+            
+        } else if seconds == 60 {
+            seconds = 0
+            minutes += 1
+            
+            if seconds < 10 && minutes < 10 {
+                
+                return timerTextField.text = "0\(minutes) : 0\(seconds)"
+                
+            } else if seconds < 10 && minutes >= 10 {
+                
+                return timerTextField.text = "\(minutes) : 0\(seconds)"
+                
+            } else if seconds >= 10 && minutes < 10 {
+                
+                return timerTextField.text = "0\(minutes) : \(seconds)"
+                
+            } else {
+                
+                return timerTextField.text = "\(minutes) : \(seconds)"
+                
+            }
+            
+        }
+        
+        
+    }
+    
+    @IBAction func startTimer(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func pauseTimer(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+    }
+    
+    @IBAction func resetTimer(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+        timerTextField.text = "00 : 00"
+        
+        seconds = 0
+        minutes = 0
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        timerTextField.text = "00 : 00"
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
